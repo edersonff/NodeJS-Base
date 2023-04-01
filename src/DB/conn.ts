5469862c6718da8d743e0e29e761ceb71c004b6a
edersonff <=>
2023-04-01 20:33:28 -0300

    fix: using mongodb

--- src/DB/conn.ts
+++ src/DB/conn.ts
@@ -1,14 +1,14 @@
 import { ConnectionOptions, DataSource, createConnection } from "typeorm";
 import { Product } from "../Entity/product.entity";
 
+const { DB_URL } = process.env;
+
 const conn: ConnectionOptions = {
-  type: "sqlite",
-  database: "./src/data/" + String(process.env.DB_NAME) + ".sqlite",
-  synchronize: true,
+  type: "mongodb",
+  url: DB_URL,
+  useNewUrlParser: true,
   logging: true,
   entities: [Product],
 };
-
-export const dataSource: DataSource = new DataSource(conn);
 
 export default createConnection(conn);
