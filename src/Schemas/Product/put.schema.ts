c2b916d45f64475af9e5b18e41c3a587a4a9d473
edersonff <=>
2023-04-01 09:48:31 -0300

    fix: putProduct schema

--- src/Schemas/Product/put.schema.ts
+++ src/Schemas/Product/put.schema.ts
@@ -1,6 +1,24 @@
 import { object, date, string } from "joi";
 
 export const putProduct = object({
-  imported_t: date().required(),
-  status: string().valid("draft", "trash", "published").required(),
+  code: string(),
+  product_name: string(),
+  quantity: string(),
+  brands: string(),
+  categories: string(),
+  labels: string(),
+  cities: string(),
+  purchase_places: string(),
+  stores: string(),
+  ingredients_text: string(),
+  traces: string(),
+  serving_size: string(),
+  serving_quantity: string(),
+  nutriscore_score: string(),
+  nutriscore_grade: string(),
+  main_category: string(),
+  image_url: string(),
+
+  imported_t: date(),
+  status: string().valid("draft", "trash", "published"),
 });
