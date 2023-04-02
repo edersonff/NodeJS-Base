a5c44b9b8076a771b02811cacdf2c33d0aeceba1
Ederson Franzen Fagundes <ederson@bemobile.tech>
2023-04-02 09:52:25 -0300

    fix: putProduct schema

--- src/Schemas/Product/put.schema.ts
+++ src/Schemas/Product/put.schema.ts
@@ -1,23 +1,10 @@
 import * as joi from "joi";
 
 export const putProduct = joi.object({
-  code: joi.string(),
-  product_name: joi.string(),
-  quantity: joi.string(),
-  brands: joi.string(),
-  categories: joi.string(),
-  labels: joi.string(),
-  cities: joi.string(),
-  purchase_places: joi.string(),
-  stores: joi.string(),
-  ingredients_text: joi.string(),
-  traces: joi.string(),
-  serving_size: joi.string(),
-  serving_quantity: joi.string(),
-  nutriscore_score: joi.string(),
-  nutriscore_grade: joi.string(),
-  main_category: joi.string(),
-  image_url: joi.string(),
+  data: joi.object().keys({
+    code: joi.number().required(),
+    name: joi.string().required(),
+  }).unknown(true),
 
   imported_t: joi.date(),
   status: joi.string().valid("draft", "trash", "published"),
