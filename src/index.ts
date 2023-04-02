54930434cef49b84f5f61a2f8152f7fa7946d5ec
Ederson Franzen Fagundes <ederson@bemobile.tech>
2023-04-02 20:02:56 -0300

    fix: cors error

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/api", router);
 
 tasks();
 app.listen(PORT, () => {
   if (DEBUG) {
-    logger.info("Server started on port 3000");
+    logger.info("Server started on port " + PORT);
   }
 });
