e7b8d60024ee82d542c04e6a2d9e79c033fd3938
Ederson Franzen Fagundes <ederson@bemobile.tech>
2023-04-02 20:04:50 -0300

    fix: remove required fields inside product.data

--- src/Schemas/Product/put.schema.ts
+++ src/Schemas/Product/put.schema.ts
@@ -1,10 +1,7 @@
 import * as joi from "joi";
 
 export const putProduct = joi.object({
-  data: joi.object().keys({
-    code: joi.number().required(),
-    name: joi.string().required(),
-  }).unknown(true),
+  data: joi.object().unknown(true),
 
   imported_t: joi.date(),
   status: joi.string().valid("draft", "trash", "published"),
