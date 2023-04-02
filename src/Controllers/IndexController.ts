7f41af239621a8afda9063adef69e7fd1ecc5896
Ederson Franzen Fagundes <ederson@bemobile.tech>
2023-04-02 09:52:05 -0300

    feat: using next_import and db status in IndexController

--- src/Controllers/IndexController.ts
+++ src/Controllers/IndexController.ts
@@ -1,12 +1,31 @@
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+import moment from "moment";
 
+const prisma = new PrismaClient();
 export default class IndexController {
   static async index(req: Request, res: Response) {
     const upTime = process.uptime();
+    const nextImport = moment().endOf("day").diff(moment(), "seconds");
+
+    const dbWrite: {
+      ok?: boolean;
+    } = await prisma.$runCommandRaw({
+      ping: 1,
+    });
+
+    const dbRead: {
+      ok?: boolean;
+    } = await prisma.$runCommandRaw({
+      ping: 1,
+    });
 
     res.send({
       up_time: upTime,
-      message: "Hello World!",
+      next_import: nextImport,
+      db_connected: await prisma.$connect(),
+      db_write: Boolean(dbWrite.ok) || false,
+      db_read: Boolean(dbRead.ok) || false,
     });
   }
 }
