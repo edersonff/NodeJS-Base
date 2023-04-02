e61b24ec3777678edf64127adb7c690d68df9e4c
Ederson Franzen Fagundes <ederson@bemobile.tech>
2023-04-02 09:13:13 -0300

    fix: data import task

--- src/Tasks/import/importData.ts
+++ src/Tasks/import/importData.ts
@@ -4,18 +4,15 @@ import zlib from "zlib";
 import fs from "fs";
 import readline from "readline";
 import https from "https";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, ProductStatus } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const url = (param: string) =>
   "https://challenges.coode.sh/food/data/json/" + param;
 
 const saveFile = async (fileUrl: any) => {
   const path = __dirname + "/tmp/" + fileUrl.split("/").pop();
-  if (!fs.existsSync(__dirname + "/tmp/")){
-    fs.mkdirSync(__dirname + "/tmp/");
-  }
   const file = fs.createWriteStream(path);
   await new Promise((resolve, reject) => {
     https.get(fileUrl, function (response) {
@@ -30,46 +27,51 @@ const saveFile = async (fileUrl: any) => {
 };
 
 export default async function importData() {
-  const product = products[0];
-  // delete all data
-  await prisma.product.deleteMany();
-  // products.map((product) => {
-  try {
-    const path = await saveFile(url(product));
-    const time = new Date().getTime();
-
-    let data = fs.readFileSync(path);
-    data = zlib.gunzipSync(data);
-
-    const jsonPath = path.split(".gz").join("");
-    fs.writeFileSync(jsonPath, data);
-
-    const fileStream = fs.createReadStream(jsonPath);
-
-    const rl = readline.createInterface({
-      input: fileStream,
-      crlfDelay: Infinity,
-    });
-
-    let count = 0;
-    let importData = [];
-    for await (const line of rl) {
-      if (count === 100) {
-        break;
-      }
-      count++;
-      const row = JSON.parse(line);
-      row.imported_t = time;
-      importData.push(JSON.parse(line));
-    }
-
-    await prisma.product.createMany({
-      data: importData,
-    });
-
-    // fs.rmSync(path);
-  } catch (err) {
-    logger.error("Erro importando dados(" + product + "): " + err);
+  if (!fs.existsSync(__dirname + "/tmp/")) {
+    fs.mkdirSync(__dirname + "/tmp/");
   }
-  // });
+
+  await prisma.product.deleteMany();
+  products.map(async (product) => {
+    try {
+      const path = await saveFile(url(product));
+      const time = new Date().getTime();
+
+      let data = fs.readFileSync(path);
+      data = zlib.gunzipSync(data);
+
+      const jsonPath = path.split(".gz").join("");
+      fs.writeFileSync(jsonPath, data);
+
+      const fileStream = fs.createReadStream(jsonPath);
+
+      const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+      });
+
+      let count = 0;
+      let importData = [];
+      for await (const line of rl) {
+        if (count === 100) {
+          break;
+        }
+        count++;
+
+        importData.push({
+          data: JSON.parse(line),
+          imported_t: time,
+          status: ProductStatus.draft,
+        });
+      }
+      await prisma.product.createMany({
+        data: importData,
+      });
+
+      fs.rmSync(path);
+      fs.rmSync(jsonPath);
+    } catch (err) {
+      logger.error("Erro importando dados(" + product + "): " + err);
+    }
+  });
 }
