999f52e36204c7ced294662ff89d91a5e0d9819a
Ederson Franzen Fagundes <ederson@bemobile.tech>
2023-04-02 09:52:51 -0300

    feat: activating cron schedule once a day(00:00)

--- src/Tasks/index.ts
+++ src/Tasks/index.ts
@@ -2,9 +2,9 @@ import cron from "node-cron";
 import importData from "./import/importData";
 
 const tasks = () => {
-  // cron.schedule("0 0 * * *", () => {
-  importData();
-  // });
+  cron.schedule("0 0 * * *", () => {
+    importData();
+  });
 };
 
 export default tasks;
