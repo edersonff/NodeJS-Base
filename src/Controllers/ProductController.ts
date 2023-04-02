1619de41d3f9b8234ca08c35da96971fd9a29d06
Ederson Franzen Fagundes <ederson@bemobile.tech>
2023-04-02 09:56:28 -0300

    feat: using pagination in ProductIndex

--- src/Controllers/ProductController.ts
+++ src/Controllers/ProductController.ts
@@ -1,60 +1,85 @@
-import { PrismaClient } from '@prisma/client'
-import { Request, Response } from 'express'
-import { putProduct } from '../Schemas/Product/put.schema'
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+import { putProduct } from "../Schemas/Product/put.schema";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export default class ProductController {
   static async index(req: Request, res: Response) {
-    const products = await prisma.product.findMany()
-    res.send(products)
+    const { index, limit, s } = req.query;
+
+    const products = await prisma.product.findMany({
+      skip: Number(index) || 0,
+      take: Number(limit) || 10,
+      where: {
+        data: JSON.parse(
+          JSON.stringify({
+            name: {
+              contains: s || "",
+            },
+          })
+        ),
+      },
+    });
+    res.send(products);
   }
 
   static async get(req: Request, res: Response) {
     const product = await prisma.product.findFirst({
-      where: {   
-        data: JSON.parse(JSON.stringify({
-          code: Number(req.params.code),
-        })),
+      where: {
+        data: JSON.parse(
+          JSON.stringify({
+            code: Number(req.params.code),
+          })
+        ),
       },
-    })
-    if (!product) return res.status(404).send("Produto não encontrado")
-    res.send(product)
+    });
+    if (!product) return res.status(404).send("Produto não encontrado");
+    res.send(product);
   }
 
   static async update(req: Request, res: Response) {
     const product = await prisma.product.findFirst({
-      where: {    
-        data: JSON.parse(JSON.stringify({
-          code: Number(req.params.code),
-        })),
+      where: {
+        data: JSON.parse(
+          JSON.stringify({
+            code: Number(req.params.code),
+          })
+        ),
       },
-    })
-    if (!product) return res.status(404).send("Produto não encontrado")
+    });
+    if (!product) return res.status(404).send("Produto não encontrado");
 
-    const { error } = putProduct.validate(req.body)
-    if (error) return res.status(400).send(error.details[0].message)
+    const { error } = putProduct.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
     const updatedProduct = await prisma.product.update({
       where: { id: product.id },
       data: req.body,
-    })
-    res.send(updatedProduct)
+    });
+    res.send(updatedProduct);
   }
 
   static async destroy(req: Request, res: Response) {
     const product = await prisma.product.findFirst({
-      where: {    
-        data: JSON.parse(JSON.stringify({
-          code: Number(req.params.code),
-        })),
+      where: {
+        data: JSON.parse(
+          JSON.stringify({
+            code: Number(req.params.code),
+          })
+        ),
       },
-    })
-    if (!product) return res.status(404).send("Produto não encontrado")
+    });
+    if (!product) return res.status(404).send("Produto não encontrado");
 
-    await prisma.product.delete({
-      where: { id: product.id },
-    })
-    res.send("Produto deletado com sucesso")
+    await prisma.product.update({
+      where: {
+        id: product.id,
+      },
+      data: {
+        status: "trash",
+      },
+    });
+    res.send("Produto deletado com sucesso");
   }
 }
